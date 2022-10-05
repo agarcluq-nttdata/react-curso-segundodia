7c53469f8522ea6915f0e88707c71f9b834fecec
agarcluq-nttdata <115019846+agarcluq-nttdata@users.noreply.github.com>
2022-10-05 09:36:11 +0200

    1 file modified, 1 added

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+const Counter = () => {
+return(
+  <div>
+  <h1>Counter</h1>
+  <button onClick={increise}>+1</button>
+  <button onClick={reduce}>+1</button>
+  <button onClick={reset}>reset</button>
+  </div>
+)
+}
+
+export default Counter;
